6b5478f18a6103c259e051abedaf8a20b3ed3f70
iOS开发_isRaining <yu478435363@163.com>
2015-06-04 10:25:19 +0800

    Initial Commit

---
+++ foldMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  foldMenu
+//
+//  Created by Code on 15/6/4.
+//  Copyright (c) 2015年 Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
